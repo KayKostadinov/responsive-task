07d5ea1929ff45f9d8acb0b3abc2cb1fc9560712
Kay Kostadinov <KayKostadinov@users.noreply.github.com>
2020-09-25 15:29:03 +0300

    Debugging form

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-
-const form = document.getElementById('form');
-const msg = document.getElementById('msg');
-const text = document.getElementById('msg-text');
-console.log('everything is working fine, nothing to see here')
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    const mail = e.target.email.value;
-    text.innerText = `${mail} has been registered`
-    msg.style.display = 'block';
-    setTimeout(() => {
-        text.innerText = ''
-        msg.style.display = 'none';
-    }, 3000)
-    e.target.email.value = ''
-})
-
+function main() {
+    const form = document.getElementById('form');
+    const msg = document.getElementById('msg');
+    const text = document.getElementById('msg-text');
+    console.log('everything is working fine, nothing to see here')
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        const mail = e.target.email.value;
+        text.innerText = `${mail} has been registered`
+        msg.style.display = 'block';
+        setTimeout(() => {
+            text.innerText = ''
+            msg.style.display = 'none';
+        }, 3000)
+        e.target.email.value = ''
+    })
+}
+main();
