5757b8a7b660ce9016fb903d122b5fb89f350a1a
Kay Kostadinov <KayKostadinov@users.noreply.github.com>
2020-09-25 14:41:36 +0300

    Created a popup message for successful form submission

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 
 const form = document.getElementById('form');
-
+const msg = document.getElementById('msg');
+const text = document.getElementById('msg-text');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-    alert(`${e.target.email.value} has been registered`)
+    const mail = e.target.email.value;
+    text.innerText = `${mail} has been registered`
+    msg.style.display = 'block';
+    setTimeout(() => {
+        text.innerText = ''
+        msg.style.display = 'none';
+    }, 3000)
+    e.target.email.value = ''
+    // alert(`${} has been registered`)
 })
 
