b3f830f8c38ee8dc7549cb33ea6854ca3cee6617
Kay Kostadinov <KayKostadinov@users.noreply.github.com>
2020-09-25 15:20:01 +0300

    Debugging form

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const form = document.getElementById('form');
 const msg = document.getElementById('msg');
 const text = document.getElementById('msg-text');
-
+console.log('everything is working fine, nothing to see here')
 form.addEventListener('submit', e => {
     e.preventDefault();
     const mail = e.target.email.value;
